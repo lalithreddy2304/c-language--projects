88397c0703d881269e7f789d8ba68b4829be7df5
P.SRI LALITH REDDY <250160226534.pedaballi@gdgu.org>
2025-10-24 18:49:05 +0530

    code for  calculator.c project

---
+++ calculator/calculator.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    char operator;
+    double num1, num2;
+
+    printf("Enter first number: ");
+    scanf("%lf", &num1);
+
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &operator);
+
+    printf("Enter second number: ");
+    scanf("%lf", &num2);
+
+    switch (operator) {
+        case '+':
+            printf("Result: %.2lf\n", num1 + num2);
+            break;
+        case '-':
+            printf("Result: %.2lf\n", num1 - num2);
+            break;
+        case '*':
+            printf("Result: %.2lf\n", num1 * num2);
+            break;
+        case '/':
+            if (num2 != 0)
+                printf("Result: %.2lf\n", num1 / num2);
+            else
+                printf("Error! Division by zero is not allowed.\n");
+            break;
+        default:
+            printf("Invalid operator! Please use +, -, *, or /\n");
+    }
+
+    return 0;
+}
